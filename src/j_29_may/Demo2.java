baa3d51f9d3e674cd2a944fef98c826fa8e3665f
suhvishal <23210008@mkcl.org>
2017-05-30 10:04:29 +0530

    -

---
+++ src/j_29_may/Demo2.java
@@ -0,0 +1,12 @@
+package j_29_may;
+
+/**
+ * Created by Administrator on 30-05-2017.
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
+
